c71f4c39f92da58c7d8a1655082c34629ecc4655
Ashvin Chauhan <ashvin.tps@gmail.com>
2017-06-29 16:11:18 +0530

    Redirect to welcome route if user already logged in

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import './App.css';
 import Routes from './routes';
 import Layout from './layout/layout';
+import { authToken } from './helper.js';
 injectTapEventPlugin();
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authToken: authToken()
+    }
+  }
+
   render() {
     return (
       <div>
         <Layout />
+        { this.state.authToken &&
+          <Redirect to="/welcome" />
+        }
       </div>
     );
   }
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,25 +1,11 @@
 import React, {Component} from 'react';
 import { Redirect, Link } from 'react-router-dom';
-import { authToken } from './helper.js';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
 
 class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      authToken: authToken()
-    }
-  }
-
   render() {
-    if (this.state.authToken) {
-      return (
-        <Redirect to="/welcome" />
-      )
-    }
-
     return (
       <div>
         <MuiThemeProvider>
